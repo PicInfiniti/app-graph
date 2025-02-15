118dc97081cc517b9430c3931c2180d808e14a39
Siavash <picinfiniti@gmail.com>
2025-02-15 10:42:13 -0500

    fix unselect

--- src/assets/js/init.js
+++ src/assets/js/init.js
@@ -1,7 +1,7 @@
 import $ from "jquery";
 import * as d3 from 'd3';
 import Graph from 'graphology'; // Import Graphology
-import { getMinAvailableNumber, getAvailableLabel, removeString } from './utils';
+import { getMinAvailableNumber, getAvailableLabel, removeString, lineIntersectsRect, pointInRect } from './utils';
 import { keyDown } from '../../main';
 import { LimitedArray, getTouchPosition } from './utils';
 import { getComponent } from "./utils";
@@ -389,41 +389,4 @@ svg.on("mousedown", function (event) {
   });
 });
 
-// Function to check if a line (edge) intersects the selection box
 
-function lineIntersectsRect(line, rect) {
-  let [x1, y1, x2, y2] = line;  // Line segment coordinates
-  let [a, b, c, d] = rect;  // Rectangle properties
-
-  // Check if the line intersects any of the rectangle's edges
-  if (lineIntersectsLine([x1, y1, x2, y2], [a, b, c, d])) {
-    return true;  // Intersection found
-  }
-  if (lineIntersectsLine([x1, y1, x2, y2], [a, d, c, b])) {
-    return true;  // Intersection found
-  }
-  return false;  // No intersection
-}
-
-// returns true if the line from (a,b)->(c,d) intersects with (p,q)->(r,s)
-function lineIntersectsLine(line1, line2) {
-  var det, gamma, lambda;
-  const [a, b, c, d] = line1
-  const [p, q, r, s] = line2
-  det = (c - a) * (s - q) - (r - p) * (d - b);
-  if (det === 0) {
-    return false;
-  } else {
-    lambda = ((s - q) * (r - a) + (p - r) * (s - b)) / det;
-    gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det;
-    return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
-  }
-};
-
-function pointInRect(a, b, x1, y1, x2, y2) {
-  if (a >= x1 && a <= x2 && b >= y1 && b <= y2) {
-    return true
-  } else {
-    return false
-  }
-}
--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -39,7 +39,11 @@ export function getAvailableLabel(n, maxLength = 3) {
 export const includesById = (array, id) => array.some(obj => obj.id === id);
 
 export function removeString(array, str) {
-  return array.filter(item => item !== str);
+  for (let i = array.length - 1; i >= 0; i--) {
+    if (array[i] === str) {
+      array.splice(i, 1);
+    }
+  }
 }
 
 export class LimitedArray {
@@ -97,4 +101,39 @@ export function getComponent(graph, node) {
 }
 
 
+export function lineIntersectsRect(line, rect) {
+  let [x1, y1, x2, y2] = line;  // Line segment coordinates
+  let [a, b, c, d] = rect;  // Rectangle properties
 
+  // Check if the line intersects any of the rectangle's edges
+  if (lineIntersectsLine([x1, y1, x2, y2], [a, b, c, d])) {
+    return true;  // Intersection found
+  }
+  if (lineIntersectsLine([x1, y1, x2, y2], [a, d, c, b])) {
+    return true;  // Intersection found
+  }
+  return false;  // No intersection
+}
+
+// returns true if the line from (a,b)->(c,d) intersects with (p,q)->(r,s)
+function lineIntersectsLine(line1, line2) {
+  var det, gamma, lambda;
+  const [a, b, c, d] = line1
+  const [p, q, r, s] = line2
+  det = (c - a) * (s - q) - (r - p) * (d - b);
+  if (det === 0) {
+    return false;
+  } else {
+    lambda = ((s - q) * (r - a) + (p - r) * (s - b)) / det;
+    gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det;
+    return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
+  }
+};
+
+export function pointInRect(a, b, x1, y1, x2, y2) {
+  if (a >= x1 && a <= x2 && b >= y1 && b <= y2) {
+    return true
+  } else {
+    return false
+  }
+}
