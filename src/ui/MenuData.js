3b1cf31939b4c224e8d8cfc5c0cd1a8696d3e750
PicInfiniti <picinfiniti@gmail.com>
2025-07-06 15:25:06 -0400

    v1.9.1

--- src/ui/MenuData.js
+++ src/ui/MenuData.js
@@ -698,7 +698,7 @@ export const menuData = [
     submenu: [
       { title: "Mouse Commnads", id: "command", name: "command-btn" },
       { title: "How to use", id: "how-to-use" },
-      { title: "About", id: "about", shortcut: "version: 1.9.0" },
+      { title: "About", id: "about", shortcut: "version: 1.9.1" },
     ],
   },
 ];
